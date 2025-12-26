744a47f321cb160703abb1a8cd5a3217326ed70c
EmreBekfilavi <embek_95@hotmail.com>
2025-12-26 08:15:24 +0300

    Fetched data from rapidapi with using axios. useSatate hooks are used and data send to components with props

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
-const Videos = () => {
-  return (
-    <div>Videos</div>
-  )
-}
+const Videos = ({ videos }) => {
 
-export default Videos
+    
+    
+
+    return (
+        <div>Videos</div>
+    );
+};
+
+export default Videos;
